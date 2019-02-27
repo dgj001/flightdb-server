ae9c1add206e09ccad6260bc61a0dbb6c69ed0cc
Dustin Johnson <dustinjohnson@fastmail.fm>
2019-02-27 11:22:11 -0500

    Empty project.

---
+++ src/main/java/my/flightdb/flightdbserver/FlightDbServerApplication.java
@@ -0,0 +1,13 @@
+package my.flightdb.flightdbserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightDbServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightDbServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/my/flightdb/flightdbserver/FlightDbServerApplicationTests.java
@@ -0,0 +1,16 @@
+package my.flightdb.flightdbserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FlightDbServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
