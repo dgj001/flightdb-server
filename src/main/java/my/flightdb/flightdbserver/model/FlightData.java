5b99de717d747efe3759f75c22bfdb8607942b08
Dustin Johnson <dustinjohnson@fastmail.fm>
2019-03-02 18:48:04 -0500

    Configured to read MySQL, fixed column name.

--- src/main/java/my/flightdb/flightdbserver/model/FlightData.java
+++ src/main/java/my/flightdb/flightdbserver/model/FlightData.java
@@ -3,6 +3,7 @@ package my.flightdb.flightdbserver.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 
 @Entity
@@ -11,6 +12,7 @@ import javax.persistence.Entity;
 public class FlightData extends BaseEntity {
     double time;
     double altitude;
+    @Column(name = "groundspeed")
     double groundSpeed;
     double latitude;
     double longitude;
